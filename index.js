ede43d4d3b7cfcadcd1c9c91548662b407091dca
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-09-05 21:16:12 +0200

    finishHomework3.2

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-
-
+let firstWindow = prompt("What IDE do you use?");
+let secondWindow = prompt("What is your name");
+let thirdWindow = prompt("How old are you?");
+console.log(`I use ${firstWindow}, my name is ${secondWindow}, i am ${thirdWindow} y.o `);
