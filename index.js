8fe4ed4c9eb51e0137a8708e7d74a4f051a397f3
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-09-05 21:50:06 +0200

    finishHomework3.3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+let mainNumber = 10369
+let fifthNumber = mainNumber % 10
+let fourthNumber = Math.floor((mainNumber / 10) % 10)
+let thirdNumber = Math.floor((mainNumber / 100) % 10)
+let secondNumber = Math.floor((mainNumber / 1000) % 10)
+let firstNumber = Math.floor(mainNumber / 10000)
+console.log(firstNumber)
+console.log(secondNumber)
+console.log(thirdNumber)
+console.log(fourthNumber)
+console.log(fifthNumber)
+
+
