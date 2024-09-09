aa61e7cc2cf8e2e66e920a3241e4f921cc9c84e2
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-09-09 23:58:22 +0200

    finishHomework4.2

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 let num = Number(prompt("Введіть трьохзначне число"));
-let thirdnum =
+let firstNum = Math.floor((num / 100) % 10);
+let secondNum = Math.floor((num / 10) % 10);
+let thirdNum = num % 10;
+if (firstNum === secondNum && secondNum === thirdNum && firstNum === thirdNum) {
+    console.log(true)} else {console.log(false)}
+if (firstNum === thirdNum || firstNum === thirdNum ||secondNum === thirdNum) {
+    console.log(true)} else {console.log(false)}
+
+
+
+
+
+
+
+
+
+
