49a95c29b8e2a491297ef2fb80d8c988ad4ca298
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-09-13 14:36:50 +0200

    finishHomework5.3

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Happy developing ✨')
+let num = +prompt("Enter a number");
+
+for (let i = 1; i <= 100; i++) {
+    if (i * i <= num) {
+        console.log(i);
+    }else {
+        break;
+    }
+}
