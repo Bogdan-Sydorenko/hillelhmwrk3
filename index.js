536725fc52eb88550c5b1f86984988aa1591c856
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-09-14 16:00:35 +0200

    finishHomework5.4

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Happy developing ✨')
+let num = prompt("Введіть ціле число:");
+
+function primeNum(n) {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+if (primeNum(num) === true) {
+    console.log(num + " є простим числом.");
+} else {
+    console.log(num + " не є простим числом.");
+}
