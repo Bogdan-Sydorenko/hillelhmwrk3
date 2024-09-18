fa46a163242cd0e35b519b801fea3616160271e1
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-09-19 00:43:26 +0200

    finishHomework6.2

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Happy developing ✨')
+let sum = 0
+let quantity = 0
+
+function average (arr) {
+
+    arr.forEach((item) => {
+        if (typeof(item) === 'number') {
+            sum += item;
+            quantity++;
+        }
+    });
+
+    if (quantity === 0) {
+        console.log('В масиві немає елементів типу (число)');
+    }
+
+    return sum / quantity;
+}
+
+const array = [3, 'word', 5, undefined, 6, 'str', true, 56, 76, 4]
+
+console.log(average(array));
