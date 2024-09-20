957327bd91bf2ed8756199ca119d5c0c89244076
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-09-20 16:28:08 +0200

    finishHomework6.3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+function removeElement(array, item) {
+    const indexItem = array.indexOf(item);
+    if (indexItem !== -1) {
+        array.splice(indexItem, 1);
+    }
+}
+
+const array = [1, 3, 4, 6, 2, 5, 7];
+
+removeElement(array,4);
+
+console.log(array);
+
