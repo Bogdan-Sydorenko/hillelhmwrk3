b6d3056206268b8cfcca7f0b39742df48c8de7bd
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-09-24 15:44:42 +0200

    finishHomework7.3

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Happy developing ✨')
+function Number() {
+    let item;
+
+    for (let i = 0; i < 10; i++) {
+        item = prompt("Введіть число більше 100:");
+
+        if (item > 100 || i === 9) {
+            console.log("Останнє введене число: " + item);
+            break;
+        } else {
+            console.log("Число повинно бути більше ста, спробуйте ще раз");
+        }
+    }
+}
+
+Number();
+
