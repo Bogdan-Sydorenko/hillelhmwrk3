7150438db1d4866f1f87858d2aeed677b916dffa
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-10-05 15:59:17 +0200

    finishHomework10.1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Happy developing ✨')
+const user = {
+    username: 'Carl',
+    age: 21,
+    city: 'Zhytomyr',
+    course: 'Front-End Pro',
+
+    getUserInfo: function () {
+        console.log(`user name: ${user.username}`);
+        console.log(`user age: ${user.age}`);
+        console.log(`user city: ${user.city}`);
+        console.log(`user course: ${user.course}`);
+    }
+};
+
+user.getUserInfo()
+
