b09dd8acdd40474290b6f79ea04ca50b612d58c9
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-10-08 20:38:24 +0200

    finishHomework11.2

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Happy developing ✨')
+const textBlock = document.getElementById('text-block');
+
+const button = document.getElementById('toggle-button');
+
+const defaultColor = 'black';
+
+const newColor = 'red';
+
+button.addEventListener('click', () => {
+    if (textBlock.style.color === newColor) {
+        textBlock.style.color = defaultColor;
+    } else {
+        textBlock.style.color = newColor;
+    }
+});
