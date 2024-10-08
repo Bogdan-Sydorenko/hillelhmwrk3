79f189df4e8022ed7db7e863899354b7b126ea3e
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-10-08 02:11:12 +0200

    finishHomework9.1

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 let company = {
     sales: [
-        {name: 'John', salary: 1000},
-        {name: 'Alice', salary: 600},
+        { name: "Hanna", salary: 2000 },
+        { name: "Alice", salary: 600 },
     ],
     development: {
         web: [
-            {name: 'Peter', salary: 2000},
-            {name: 'Alex', salary: 1800}
+            { name: "Peter", salary: 2000 },
+            { name: "Alex", salary: 1800 },
         ],
         internals: [
-            {name: 'Jack', salary: 1300},
-        ]
-    }
+            { name: "Jack", salary: 1300 },
+        ],
+    },
 };
+
+function calculateTotalSalary(departament) {
+    let totalSalary = 0;
+
+    if (Array.isArray(departament)) {
+        departament.forEach(employee => {
+            totalSalary += employee.salary;
+        });
+    }
+
+    else {
+        for (let subDepartament in departament) {
+            totalSalary += calculateTotalSalary(departament[subDepartament]);
+        }
+    }
+
+    return totalSalary;
+}
+
+let total = calculateTotalSalary(company);
+
+console.log("Total salary:", total);
 
