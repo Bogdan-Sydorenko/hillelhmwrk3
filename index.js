1025a13adfa3c48496ad92042a919e9eb837aa3d
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-10-08 20:59:32 +0200

    finishHomework11.3

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Happy developing ✨')
+const imageFiles = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg', 'images/6.jpg', 'images/7.jpg', 'images/8.jpg', 'images/9.jpg'];
+
+const randomIndex = Math.floor(Math.random() * imageFiles.length);
+
+const randomImage = imageFiles[randomIndex];
+
+document.getElementById('randomImage').src = randomImage;
