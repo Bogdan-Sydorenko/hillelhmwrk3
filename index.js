92241f4b4e0a28e0e6edd605b943bcce85525bbd
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-10-14 13:50:35 +0200

    finishHomework12.1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+let userLink = ''
+function enterLink() {
+    userLink = prompt('Enter a link')
+    if (userLink && !userLink.startsWith('http')) {
+        userLink = 'https://' + userLink
+    }
+}
 
+function activateLink () {
+    if (userLink) {
+        window.location.href = userLink
+    }else {
+        console.log("First enter the link.")
+    }
+}
+
+document.querySelector('#promptButton').addEventListener('click', enterLink)
+document.querySelector('#linkButton').addEventListener('click', activateLink)
