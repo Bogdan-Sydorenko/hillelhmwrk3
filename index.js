fbd1c2b1ce316d6d5a195da0ff6c280a3a409ef7
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-10-19 17:13:23 +0200

    finishHomework12.2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const buttonElem = document.querySelector("#container")
+
+buttonElem.addEventListener("click", function (event) {
+    if (event.target.tagName === "BUTTON") {
+        console.log(`You click on ${event.target.id}`)
+    }
+})
+
 
