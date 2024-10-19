c825bffbf379bbca599e35827b1c79c529ad2b0c
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-10-19 17:58:19 +0200

    finishHomework12.3

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+// Отримуємо посилання на необхідні елементи
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('TaskButton');
+const taskList = document.getElementById('taskList');
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+        const li = document.createElement('li');
+        li.innerHTML = `${taskText} <button class="delete">Delete</button>`;
+
+        taskList.appendChild(li);
+
+        taskInput.value = '';
+    }
+}
+
+addTaskButton.addEventListener('click', addTask);
+
+taskList.addEventListener('click', function(event) {
+    if (event.target.classList.contains('delete')) {
+        const li = event.target.parentElement;
+        taskList.removeChild(li);
+    }
+});
 
