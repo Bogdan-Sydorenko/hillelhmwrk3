4464f5674dabd422fd11a0cb84bd1249ee390d50
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-10-29 15:38:18 +0100

    finishHomework17.2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+class Coach {
+
+    constructor(name, specialization, rating) {
+        this.name = name;
+        this.specialization = specialization;
+        this.rating = rating;
+    }
+
+    displayInfo() {
+        console.log(`Ім'я тренера: ${this.name}`);
+        console.log(`Спеціалізація: ${this.specialization}`);
+        console.log(`Рейтинг: ${this.rating}`);
+    }
+}
+
+const coach1 = new Coach('Alice', 'Fitness',5.2);
+const coach2 = new Coach('Bohdan', 'Yoga',9.8);
+
+coach1.displayInfo();
+coach2.displayInfo();
 
