57b58a4142a00c064a96b6ac0e45042e2608b0dd
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-10-29 15:32:45 +0100

    finishHomework17.1

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+class Calculator {
+
+    add(a, b) {
+        return a + b;
+    }
+
+    minus(a, b) {
+        return a - b;
+    }
+
+    multiply(a, b) {
+        return a * b;
+    }
+
+    degree(a, b) {
+        if (b === 0) {
+            console.log("Ділення на нуль неможливе!");
+        } else
+        return a / b;
+    }
+}
+
+const calculator = new Calculator();
+
+console.log (calculator.add(32, 43));
+console.log (calculator.minus(179,79));
+console.log (calculator.multiply(3,30));
+console.log (calculator.degree(50,2));
+console.log (calculator.degree(5,0));
+
 
