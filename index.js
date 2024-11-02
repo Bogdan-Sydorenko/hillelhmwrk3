a38815dc5b445ec75782654cb3b40a1e9d17d1c9
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-11-02 15:02:37 +0100

    finishHomework18.1

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+let timeLeft = 85;
+
+function updateTimer() {
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    document.getElementById('timer').innerText = `${minutes}:${seconds}`;
+
+    if (timeLeft > 0) {
+        timeLeft--;
+    } else {
+        clearInterval(timerInterval);
+    }
+}
+
+const timerInterval = setInterval(updateTimer, 1000);
 
