7c43ef7f8196da830af7625d9dcfbfcd3343a915
Bogdan-Sydorenko <b.sidorenko17@gmail.com>
2024-11-07 16:31:32 +0100

    finishHomework19.1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const apiKey = '43e3c5bbc5e5bcf640cbaab1d9876893';
+const city = 'Odessa';
 
+async function fetchWeatherData() {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=en`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        document.getElementById('city').textContent = data.name;
+        document.getElementById('temperature').textContent = Math.round(data.main.temp);
+        document.getElementById('description').textContent = data.weather[0].description;
+    } catch (error) {
+        console.error('Помилка при отриманні даних:', error);
+        document.getElementById('weather-info').textContent = 'Помилка з’єднання';
+    }
+}
+
+fetchWeatherData();
+
+document.querySelector('button').onclick = fetchWeatherData;
